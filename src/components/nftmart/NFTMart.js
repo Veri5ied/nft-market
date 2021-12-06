27b507ae8bcc2627c79a4f3ccb6fd64fd90d749c
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2021-12-06 21:33:00 +0100

    dashbord layout

--- src/components/nftmart/NFTMart.js
+++ src/components/nftmart/NFTMart.js
@@ -1,7 +1,22 @@
+import FirstImage from "../../assets/images/unsplash_8uZPynIu-rQ.png";
+import SecondImage from "../../assets/images/unsplash_OlTjeydUpQw.png";
+import ThirdImage from "../../assets/images/unsplash_pVq6YhmDPtk.png";
+import FourthImage from "../../assets/images/unsplash_RnCPiXixooY.png";
+import FifthImage from "../../assets/images/unsplash_tMbQpdguDVQ.png";
+import SixthImage from "../../assets/images/unsplash_tZCrFpSNiIQ.png";
+import SeventhImage from "../../assets/images/unsplash_VS_kFx4yF5g.png";
+import EighthImage from "../../assets/images/Top.png";
+import "./NFTMart.css"
+
 function NFTMart() {
     return (
         <div className="nft__mart">
-            HELLO NFT MARKET...
+            <div className="nftmart__top">
+                
+            </div>
+            <div className="nftmart__body">
+
+            </div>
         </div>
     )
 }
