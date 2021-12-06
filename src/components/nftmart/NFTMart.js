c5eb6625511b9666e576b780ee44404567a0a3b8
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2021-12-06 22:01:27 +0100

    dashboard body

--- src/components/nftmart/NFTMart.js
+++ src/components/nftmart/NFTMart.js
@@ -6,19 +6,40 @@ import FifthImage from "../../assets/images/unsplash_tMbQpdguDVQ.png";
 import SixthImage from "../../assets/images/unsplash_tZCrFpSNiIQ.png";
 import SeventhImage from "../../assets/images/unsplash_VS_kFx4yF5g.png";
 import EighthImage from "../../assets/images/Top.png";
-import "./NFTMart.css"
+import Arrow from "../../assets/images/akar-icons_chevron-down.png";
+import Grid from "../../assets/images/Icon 1.png";
+import Tiles from "../../assets/images/Icon 2.png";
+import "./NFTMart.css";
 
 function NFTMart() {
-    return (
-        <div className="nft__mart">
-            <div className="nftmart__top">
-                
-            </div>
-            <div className="nftmart__body">
-
-            </div>
+  return (
+    <div className="nft__mart">
+      <div className="nftmart__top mt-4">
+        <h1 className="nftmart__title">Cryptographics</h1>
+        <button className="nftcreate">Create Item</button>
+      </div>
+      <div className="nftmart__center mt-2">
+        <div className="nftmart__btn">
+          <button className="recent">
+            Recent <img src={Arrow} alt="arrow" />
+          </button>
+          <button className="low">
+            Low to High
+            <img src={Arrow} alt="arrow" />
+          </button>
+          <button className="all">
+            All
+            <img src={Arrow} alt="arrow" />
+          </button>
         </div>
-    )
+        <div className="nftleft__btn">
+          <img src={Tiles} alt="tiles" />
+          <img src={Grid} alt="grid" />
+        </div>
+      </div>
+      <div className="nftmart__body"></div>
+    </div>
+  );
 }
 
 export default NFTMart;
